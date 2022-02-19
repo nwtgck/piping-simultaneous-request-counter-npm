8c02c269e0e53a0d3cdaa9af37f414843bf6eed2
Ryo Ota <nwtgck@nwtgck.org>
2022-02-20 05:54:13 +0900

    show console.log in textarea

--- operational-test/index.ts
+++ operational-test/index.ts
@@ -1,10 +1,18 @@
 
-import {measureSimultaneousRequestCount} from '../src/index';
+import {measureSimultaneousRequestCount} from '../src';
 
 const serverUrl = "https://ppng.herokuapp.com"; // (HTTP/1.1)
 // const serverUrl = "http://localhost:8080"; // Local Piping Server
 // const serverUrl = "https://piping-47q675ro2guv.runkit.sh"; // (HTTP/1.1)
 // const serverUrl = "https://ppng.ml"; // (HTTP/2)
+
+const originalConsoleLog = console.log;
+const consoleTextArea = document.getElementById('console_textarea') as HTMLTextAreaElement
+
+console.log = (...args) => {
+  consoleTextArea.value += `${args.join(" ")}\n`;
+  originalConsoleLog.apply(args);
+};
 
 (async () => {
   console.log('start');
